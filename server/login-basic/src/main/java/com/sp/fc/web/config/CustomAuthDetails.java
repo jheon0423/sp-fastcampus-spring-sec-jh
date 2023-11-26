3a8e6e345858f86978dd7183acb86a10b0a59fc5
jheon0423 <jh0423lee@naver.com>
2023-11-26 22:23:50 +0900

    commit 1126

---
+++ server/login-basic/src/main/java/com/sp/fc/web/config/CustomAuthDetails.java
@@ -0,0 +1,22 @@
+package com.sp.fc.web.config;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CustomAuthDetails implements AuthenticationDetailsSource<HttpServletRequest, RequestInfo> {
+
+    @Override
+    public RequestInfo buildDetails(HttpServletRequest request) {
+        return RequestInfo.builder()
+                .remoteIp(request.getRemoteAddr())
+                .sessionId(request.getSession().getId())
+                .loginTime(LocalDateTime.now())
+                .build();
+    }
+
+}
